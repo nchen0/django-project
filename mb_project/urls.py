6e0a317178d73f642bf2b3091a1ce159a27ea363
Nicky Chen <nicky.chen0@gmail.com>
2018-09-19 19:05:18 -0700

    serializer

--- mb_project/urls.py
+++ mb_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers
+from api.views import ListSongsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
